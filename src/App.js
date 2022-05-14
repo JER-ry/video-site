1dbcab5e87dc2a6bfbdcc7d9c78c64709d796e69
Jerry <jer.jerry@outlook.com>
2022-05-14 23:19:33 +0800

    User page working

--- src/App.js
+++ src/App.js
@@ -11,12 +11,111 @@ function Tab(props) {
   );
 }
 
-function User(props) {
-  return (
-    <div className="flex flex-wrap h-min gap-6">
 
-    </div>
-  );
+class User extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: 1, // 0 - login, 1 - register, 2 - logged in
+    }
+  }
+
+  handleRegister() {
+    this.setState({
+      status: 1,
+    })
+  }
+
+  render() {
+    if (this.state.status === 0)
+      return (
+        <div className="flex w-full h-full items-center justify-center">
+          <div className="overflow-hidden bg-white rounded-lg shadow-md">
+            <div className="px-10 pt-10 pb-8 space-y-6">
+              <h2 className="text-2xl font-bold text-left text-gray-700">Welcome to VideoLab</h2>
+              <form className="flex justify-between">
+                <input className="w-[65%] px-4 py-3 text-gray-700 placeholder-gray-400 bg-white border rounded-md duration-200 focus:border-gray-300 focus:ring-opacity-40 focus:ring focus:ring-gray-300 focus:outline-none" type="ID" placeholder="User ID" aria-label="ID" />
+                <button className="w-[30%] px-4 py-3 text-center text-white duration-200 bg-gray-700 rounded hover:bg-gray-600 focus:ring-opacity-40 focus:ring focus:ring-gray-300 focus:outline-none" type="button">Login</button>
+              </form>
+            </div>
+            <div className="items-center justify-center py-4 text-center bg-gray-50">
+              <button className="mx-1 text-sm font-bold duration-200 text-gray-400 hover:text-gray-500" onClick={() => this.handleRegister()}>Register here</button><span className="text-sm text-gray-400">if you don't have an account.</span>
+            </div>
+          </div>
+        </div>
+      );
+    else if (this.state.status === 1)
+      return (
+        <div className="flex w-full h-full items-center justify-center">
+          <div className="p-10 space-y-6 overflow-hidden bg-white rounded-lg shadow-md">
+            <h2 className="text-2xl font-bold text-left text-gray-700">Choose 3 categories of your interest</h2>
+            <form className="flex flex-col gap-8">
+              <div class="flex flex-wrap h-min gap-6">
+                <div class="relative">
+                  <input class="hidden group peer" type="checkbox" name="interstedCategories" value="1" id="1" />
+                  <label class="block p-4 text-sm font-medium transition-colors border border-gray-100 rounded-lg shadow-sm cursor-pointer peer-checked:border-gray-500 hover:bg-gray-50 peer-checked:ring-1 peer-checked:ring-gray-500" for="1">
+                    Short
+                  </label>
+                </div>
+
+                <div class="relative">
+                  <input class="hidden group peer" type="checkbox" name="interstedCategories" value="2" id="2" />
+                  <label class="block p-4 text-sm font-medium transition-colors border border-gray-100 rounded-lg shadow-sm cursor-pointer peer-checked:border-gray-500 hover:bg-gray-50 peer-checked:ring-1 peer-checked:ring-gray-500" for="2">
+                    Movie
+                  </label>
+                </div>
+
+                <div class="relative">
+                  <input class="hidden group peer" type="checkbox" name="interstedCategories" value="3" id="3" />
+                  <label class="block p-4 text-sm font-medium transition-colors border border-gray-100 rounded-lg shadow-sm cursor-pointer peer-checked:border-gray-500 hover:bg-gray-50 peer-checked:ring-1 peer-checked:ring-gray-500" for="3">
+                    Seems normal
+                  </label>
+                </div>
+
+                <div class="relative">
+                  <input class="hidden group peer" type="checkbox" name="interstedCategories" value="4" id="4" />
+                  <label class="block p-4 text-sm font-medium transition-colors border border-gray-100 rounded-lg shadow-sm cursor-pointer peer-checked:border-gray-500 hover:bg-gray-50 peer-checked:ring-1 peer-checked:ring-gray-500" for="4">
+                    Rubbish
+                  </label>
+                </div>
+                <div class="relative">
+                  <input class="hidden group peer" type="checkbox" name="interstedCategories" value="5" id="5" />
+                  <label class="block p-4 text-sm font-medium transition-colors border border-gray-100 rounded-lg shadow-sm cursor-pointer peer-checked:border-gray-500 hover:bg-gray-50 peer-checked:ring-1 peer-checked:ring-gray-500" for="5">
+                    Idk
+                  </label>
+                </div>
+
+                <div class="relative">
+                  <input class="hidden group peer" type="checkbox" name="interstedCategories" value="6" id="6" />
+                  <label class="block p-4 text-sm font-medium transition-colors border border-gray-100 rounded-lg shadow-sm cursor-pointer peer-checked:border-gray-500 hover:bg-gray-50 peer-checked:ring-1 peer-checked:ring-gray-500" for="6">
+                    Sci-fi
+                  </label>
+                </div>
+
+                <div class="relative">
+                  <input class="hidden group peer" type="checkbox" name="interstedCategories" value="7" id="7" />
+                  <label class="block p-4 text-sm font-medium transition-colors border border-gray-100 rounded-lg shadow-sm cursor-pointer peer-checked:border-gray-500 hover:bg-gray-50 peer-checked:ring-1 peer-checked:ring-gray-500" for="7">
+                    Music
+                  </label>
+                </div>
+
+                <div class="relative">
+                  <input class="hidden group peer" type="checkbox" name="interstedCategories" value="8" id="8" />
+                  <label class="block p-4 text-sm font-medium transition-colors border border-gray-100 rounded-lg shadow-sm cursor-pointer peer-checked:border-gray-500 hover:bg-gray-50 peer-checked:ring-1 peer-checked:ring-gray-500" for="8">
+                    Anime
+                  </label>
+                </div>
+              </div>
+
+              <button className="py-3 text-center text-white duration-200 bg-gray-700 rounded hover:bg-gray-600 focus:ring-opacity-40 focus:ring focus:ring-gray-300 focus:outline-none" type="button">Submit</button>
+            </form>
+
+          </div>
+        </div>
+      );
+    else
+      return {};
+  }
 }
 
 function VideoPreview(props) {
@@ -70,6 +169,15 @@ class VideoList extends React.Component {
       anythingToRecommend: true,
       playing: false,
       videoToPlay: null,
+    }
+  }
+
+  componentDidMount() {
+    if (videoList.length === 0) {
+      recommendMore();
+      this.setState({
+        videoList: videoList,
+      });
     }
   }
 
@@ -140,7 +248,7 @@ class Player extends React.Component {
   render() {
     return (
       <div className="flex flex-col w-full h-full items-center gap-6">
-        <div className="flex w-[90%] h-[90%] bg-white rounded-md shadow-md overflow-hidden duration-200 text-gray-300 text-center">
+        <div className="flex w-[90%] h-[90%] bg-white rounded-md shadow-md overflow-hidden text-gray-300 text-center">
           <iframe className="w-full" src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
         <div className="inline-flex place-content-between items-center font-medium text-base w-[90%] h-[10%] bg-white rounded-md overflow-hidden shadow-md">
@@ -164,7 +272,6 @@ class Player extends React.Component {
 class App extends React.Component {
   constructor(props) {
     super(props);
-    // Update videoList here
     this.state = {
       tabList: [
         {
@@ -176,7 +283,7 @@ class App extends React.Component {
           icon: <svg width="18" height="18" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M14.53.47a.75.75 0 0 1 0 1.06l-2.72 2.72H17c.966 0 1.75.784 1.75 1.75v10.5A1.75 1.75 0 0 1 17 18.25H3a1.75 1.75 0 0 1-1.75-1.75V6c0-.966.784-1.75 1.75-1.75h4.894L5.85 2.581a.75.75 0 0 1 .948-1.162L9.95 3.99l3.52-3.52a.75.75 0 0 1 1.061 0ZM9.997 5.75H17a.25.25 0 0 1 .25.25v10.5a.25.25 0 0 1-.25.25H3a.25.25 0 0 1-.25-.25V6A.25.25 0 0 1 3 5.75h6.997ZM7.5 13.25a.75.75 0 0 0 0 1.5h5a.75.75 0 0 0 0-1.5h-5Z" fill="#0C0D0F"></path></svg>,
         }
       ],
-      currentTab: 1,
+      currentTab: 0,
     }
   };
 
@@ -205,85 +312,96 @@ class App extends React.Component {
   }
 }
 
-var videoList = [
-  {
-    id: {},
-    title: "映画『ゆるキャン△』2022年初夏、全国ロードショー",
-    cover: require('./img/test2.jpg'),
-    lengthStr: "01:11",
-    category: 'Movie',
-    liked: false,
-    recommended: false,
-  },
-  {
-    id: {},
-    title: "Happy New Year 2022",
-    cover: require('./img/test.jpg'),
-    lengthStr: "02:24",
-    category: 'Advertisement',
-    liked: false,
-    recommended: false,
-  },
-  {
-    id: {},
-    title: "I forgot it",
-    cover: require('./img/test3.jpg'),
-    lengthStr: "00:06",
-    viewCount: 18,
-    category: 'Short',
-    liked: false,
-    recommended: false,
-  },
-  {
-    id: {},
-    title: "Something?",
-    cover: require('./img/test4.jpg'),
-    lengthStr: "92:01",
-    category: 'Short',
-    liked: false,
-    recommended: false,
-  },
-];
-
-var videoToRecommend = [
-  {
-    id: {},
-    title: "Rubbish bin",
-    cover: require('./img/test5.jpg'),
-    lengthStr: "03:46",
-    category: 'Rubbish',
-    liked: false,
-    recommended: true,
-  },
-  {
-    id: {},
-    title: "CSSAUG cares you",
-    cover: require('./img/test6.jpg'),
-    lengthStr: "05:20",
-    category: 'Rubbish',
-    liked: false,
-    recommended: true,
-  },
-  {
-    id: {},
-    title: "A photo, where there are mountains, lakes and plants, but not sun yet",
-    cover: require('./img/test7.jpg'),
-    lengthStr: "01:11",
-    category: 'Nature',
-    liked: false,
-    recommended: true,
-  },
-]
+var videoList = [];
 
 function recommendMore() {
-  // Update videoToRecommend here
+  const videoToRecommend = recommendMoreUpdate(); // For demonstration purposes only
   if (videoToRecommend.length === 0)
     return false;
   else {
     videoList = videoList.concat(videoToRecommend);
-    videoToRecommend = [];
-    return true
+    return true;
   }
 }
+
+/* -------------Below: For demonstration purposes only------------- */
+var t = -1;
+var sim = [
+  [
+    {
+      id: {},
+      title: "Happy New Year 2022",
+      cover: require('./img/test.jpg'),
+      lengthStr: "02:24",
+      category: 'Advertisement',
+      liked: false,
+      recommended: false,
+    },
+    {
+      id: {},
+      title: "映画『ゆるキャン△』2022年初夏、全国ロードショー",
+      cover: require('./img/test2.jpg'),
+      lengthStr: "01:11",
+      category: 'Movie',
+      liked: false,
+      recommended: false,
+    },
+    {
+      id: {},
+      title: "I forgot it",
+      cover: require('./img/test3.jpg'),
+      lengthStr: "00:06",
+      viewCount: 18,
+      category: 'Short',
+      liked: false,
+      recommended: false,
+    },
+  ],
+  [
+    {
+      id: {},
+      title: "Something?",
+      cover: require('./img/test4.jpg'),
+      lengthStr: "92:01",
+      category: 'Short',
+      liked: false,
+      recommended: false,
+    }, {
+      id: {},
+      title: "Rubbish bin",
+      cover: require('./img/test5.jpg'),
+      lengthStr: "03:46",
+      category: 'Rubbish',
+      liked: false,
+      recommended: true,
+    },
+  ],
+  [
+    {
+      id: {},
+      title: "CSSAUG cares you",
+      cover: require('./img/test6.jpg'),
+      lengthStr: "05:20",
+      category: 'Rubbish',
+      liked: false,
+      recommended: true,
+    },
+    {
+      id: {},
+      title: "A photo, where there are mountains, lakes and plants, but not sun yet",
+      cover: require('./img/test7.jpg'),
+      lengthStr: "01:11",
+      category: 'Nature',
+      liked: false,
+      recommended: true,
+    },
+  ],
+]
+
+function recommendMoreUpdate() {
+  t += 1;
+  return t < sim.length ? sim[t] : [];
+}
+/* -------------Above: For demonstration purposes only------------- */
 
 export default App;
